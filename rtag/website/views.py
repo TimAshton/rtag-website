b0e26ac14c3e6db5616191664e86e953fa84d314
Tim <tim.ashton.la@gmail.com>
2026-03-15 13:32:56 -0700

    Implement a Tailwind UI.

--- rtag/website/views.py
+++ rtag/website/views.py
@@ -4,8 +4,10 @@ from django.shortcuts import render
 def home(req):
     return render(req, 'home/root.html')
 
+
 def about(req):
     return render(req, 'about/root.html')
+
 
 def rules(req):
     return render(req, 'rules/root.html')
